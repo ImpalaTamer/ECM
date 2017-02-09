8723db4baf1992d5e232064d44a6e77d7b0a1a1e
ImpalaTamer <bamgag@gmail.com>
2017-02-09 14:54:22 -0500

    added basic UI xml
    added, title, contact entry, frequency entry, notifications switch, save button

--- app/src/main/java/com/impalatamer/ecm/MainActivity.java
+++ app/src/main/java/com/impalatamer/ecm/MainActivity.java
@@ -3,6 +3,10 @@ package com.impalatamer.ecm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//TODO: Figure out how to generate notifications
+//TODO: How to access contacts or add number manually
+//TODO: Add basic UI
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
