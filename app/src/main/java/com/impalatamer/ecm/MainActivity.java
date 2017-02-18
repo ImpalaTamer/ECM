8fd29728134ffde33dfaa80872819a1f88c49ece
ImpalaTamer <bamgag@gmail.com>
2017-02-17 22:55:22 -0500

    added contact permissions request for marshmallow and later also add logic to get contacts from content provider and set adapter with ui

--- app/src/main/java/com/impalatamer/ecm/MainActivity.java
+++ app/src/main/java/com/impalatamer/ecm/MainActivity.java
@@ -1,19 +1,85 @@
 package com.impalatamer.ecm;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
-//TODO: Figure out how to generate notifications
-//TODO: How to access contacts or add number manually
-//TODO: Add basic UI
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //TODO make sure to understand this code
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+            //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
+
+        } else {
+
+            getContacts();
+
+        }
+
+    }
+
+    private void getContacts() {
+
+        Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI, new String[] { ContactsContract.Data.DISPLAY_NAME }, null, null, null );
+
+        //TODO what if a contact is added while the application is runnning?
+
+        ArrayList contacts = new ArrayList<String>();
+
+        c.moveToFirst();
+
+        while(!c.isAfterLast()) {
+
+            //TODO Why is DISPLAY_NAME string and not just a number to represent the column?
+            contacts.add(c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
+            c.moveToNext();
+
+
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, contacts);
+
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.enter_contact);
+
+        textView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission is granted
+                getContacts();
+
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+            }
+
+        }
     }
 
 
 }
+
+
