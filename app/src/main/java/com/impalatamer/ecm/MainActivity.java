1194403b2fe5ed7856190c31e0a334a098720689
ImpalaTamer <bamgag@gmail.com>
2017-02-19 15:16:46 -0500

    wired Save button to grab field data and added listener

--- app/src/main/java/com/impalatamer/ecm/MainActivity.java
+++ app/src/main/java/com/impalatamer/ecm/MainActivity.java
@@ -7,21 +7,35 @@ import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    //TODO are my visibilty modifiers good?
+    //TODO can I set variables here?
+
     private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+
+    //TODO store these in a bundle so they are saved during configuration changes
+    private static String contactValue;
+    private static boolean notificationsSwitchValue;
+    private static int frequencyValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //TODO set notifications switch to state stored in bundle
 
         //TODO make sure to understand this code
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
@@ -30,21 +44,42 @@ public class MainActivity extends AppCompatActivity {
             //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
 
         } else {
-
             getContacts();
-
         }
+
+        final Button saveBtn = (Button) findViewById(R.id.save_button);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                //TODO grab state of contact field
+                AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.enter_contact);
+                contactValue = textView.getText().toString();
+
+                //grab state of frequency field
+                EditText frequency = (EditText) findViewById(R.id.enter_frequency);
+                frequencyValue = Integer.parseInt(frequency.getText().toString());
+
+                //grab state of switch
+                Switch notificationsSwitch = (Switch) findViewById(R.id.notifcations_switch);
+                notificationsSwitchValue = notificationsSwitch.isChecked();
+
+            }
+
+        });
+
 
     }
 
     private void getContacts() {
+
+        //TODO what is cursor loader for? I dont seem to need it here?
 
         Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI, new String[] { ContactsContract.Data.DISPLAY_NAME }, null, null, null );
 
         //TODO what if a contact is added while the application is runnning?
 
         ArrayList contacts = new ArrayList<String>();
-
         c.moveToFirst();
 
         while(!c.isAfterLast()) {
@@ -52,16 +87,11 @@ public class MainActivity extends AppCompatActivity {
             //TODO Why is DISPLAY_NAME string and not just a number to represent the column?
             contacts.add(c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
             c.moveToNext();
-
-
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, contacts);
-
         AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.enter_contact);
-
         textView.setAdapter(adapter);
-
     }
 
     @Override
@@ -73,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 getContacts();
 
             } else {
-                Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Need to access your Contacts in order to retrieve a Contact's number for you to memorize", Toast.LENGTH_SHORT).show();
             }
 
         }
