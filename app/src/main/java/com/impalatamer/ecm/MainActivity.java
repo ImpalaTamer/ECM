2789aa8bfddfcf374b40f674e2d16cad88e99bcc
ImpalaTamer <bamgag@gmail.com>
2017-02-20 17:13:00 -0500

    Working prototype, added timer for notifications

--- app/src/main/java/com/impalatamer/ecm/MainActivity.java
+++ app/src/main/java/com/impalatamer/ecm/MainActivity.java
@@ -13,12 +13,13 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
     private static String contactValue;
     private static boolean notificationsSwitchValue;
     private static int frequencyValue;
+
+    private static Timer timer;
 
     NotificationCompat.Builder mBuilder;
 
@@ -48,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
 
         } else {
-            getContacts2();
+            getContacts();
         }
 
         final Button saveBtn = (Button) findViewById(R.id.save_button);
@@ -59,74 +62,74 @@ public class MainActivity extends AppCompatActivity {
                 //TODO grab state of contact field
                 AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.enter_contact);
                 contactValue = textView.getText().toString();
+                mBuilder.setContentTitle("Remember this: " + contactValue);
+                //mBuilder.setContentText("Remember this: " + contactValue); //TODO why do i need this here?
 
                 //TODO fix this value
+                //TODO make input field always center
                 //grab state of frequency field
                 EditText frequency = (EditText) findViewById(R.id.enter_frequency);
-                //frequencyValue = Integer.parseInt(frequency.getText().toString());
+                frequencyValue = Integer.parseInt(frequency.getText().toString());
+                frequencyValue = frequencyValue * 60000; //convert minutes to ms
 
                 //grab state of switch
-                Switch notificationsSwitch = (Switch) findViewById(R.id.notifcations_switch);
+                Switch notificationsSwitch = (Switch) findViewById(R.id.notifications_switch);
                 notificationsSwitchValue = notificationsSwitch.isChecked();
 
-                //TODO found compatibility solution in stackoverflow, where is this in android documentation?
-                NotificationManagerCompat n = NotificationManagerCompat.from(getApplicationContext());
-                n.notify(1, mBuilder.build());
+                //TODO is this necessary?
+                if(timer != null){
+                    timer.cancel();
+                    timer.purge();
+                }
 
+                timer = new Timer(true); //TODO isDaemon = true, does this enhance performance?
+
+                TimerTask tt = new TimerTask() {
+                    @Override
+                    public void run() {
+
+                        //TODO found compatibility solution in stackoverflow, where is this in android documentation?
+                        NotificationManagerCompat n = NotificationManagerCompat.from(getApplicationContext());
+                        n.notify(1, mBuilder.build());
+
+                        //TODO how come i can access mBuilder here?
+                    }
+                };
+
+                timer.scheduleAtFixedRate(tt, frequencyValue, frequencyValue);
+
+                //toast feedback when save button is pressed
+                Toast.makeText(getApplicationContext(), "Saved!", Toast.LENGTH_SHORT).show();
             }
 
         });
 
         //TODO build notification system
+        Toast.makeText(getApplicationContext(), "onCreate notification builder", Toast.LENGTH_SHORT);
         mBuilder = new NotificationCompat.Builder(this);
         mBuilder.setSmallIcon(R.mipmap.ic_launcher);
-        mBuilder.setContentTitle("Memorize this!");
-        mBuilder.setContentText(contactValue);
+        //mBuilder.setContentTitle("hmm");
+        //mBuilder.setContentText("Memorize this: " + contactValue);
 
     }
 
+    //TODO need to understand this, apparently it is much faster query
     private void getContacts() {
 
-        //TODO what is cursor loader for? I dont seem to need it here?
-
-        Cursor c = getContentResolver().query(ContactsContract.Data.CONTENT_URI, new String[] { ContactsContract.Data.DISPLAY_NAME }, null, null, null );
-
-        //TODO what if a contact is added while the application is runnning?
-
-        ArrayList contacts = new ArrayList<String>();
-        c.moveToFirst();
-
-        while(!c.isAfterLast()) {
-
-            //TODO Why is DISPLAY_NAME string and not just a number to represent the column?
-            contacts.add(c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)));
-            c.moveToNext();
-        }
-
-        c.close();
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, contacts);
-        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.enter_contact);
-        textView.setAdapter(adapter);
-    }
-
-    //TODO need to understand this
-    private void getContacts2() {
-
         ArrayList contacts = new ArrayList<String>();
 
-        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        Cursor cur = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
 
-        while (phones.moveToNext())
+        while (cur.moveToNext())
         {
-            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            String name = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = cur.getString(cur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-            contacts.add(name + "   " + phoneNumber);
+            contacts.add(name + " " + phoneNumber);
 
         }
 
-        phones.close();
+        cur.close();
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, contacts);
         AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.enter_contact);
@@ -139,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Permission is granted
-                getContacts2();
+                getContacts();
 
             } else {
                 Toast.makeText(this, "Need to access your Contacts in order to retrieve a Contact's number for you to memorize", Toast.LENGTH_SHORT).show();
